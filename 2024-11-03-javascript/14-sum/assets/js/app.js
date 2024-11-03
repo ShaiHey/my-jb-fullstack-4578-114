5afab9b269f0bead5d95da43edf25bc116701b27
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 15:23:01 +0200

    initial

---
+++ 2024-11-03-javascript/14-sum/assets/js/app.js
@@ -0,0 +1,11 @@
+let i = 1;
+let sum = 0;
+
+while (i !== 0) {
+    let num = +prompt("Please enter a number");
+    sum+=num;
+    i = num;
+}
+
+// console.log(`Sum is : ${sum}`)
+alert(`Sum is : ${sum}`)
