d21a30c86c14dfa174d8f011fa1ba07c57eb3f10
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 15:12:48 +0200

    initial

--- 2024-11-03-javascript/12-power-2.0/assets/js/app.js
+++ 2024-11-03-javascript/12-power-2.0/assets/js/app.js
@@ -1,10 +1,17 @@
-let i = 1
+// let i = 1
 
-while (i !== 0) {
-    let num = +prompt("Please enter a number")
-    const result = num ** 2
-    if(num !== 0){
-        console.log(result)
-    }
-    i = num
+// while (i !== 0) {
+//     let num = +prompt("Please enter a number")
+//     const result = num ** 2
+//     if(num !== 0){
+//         console.log(result)
+//     }
+//     i = num
+// }
+
+let num = +prompt("Please enter a number");
+
+while (num !== 0) {
+    console.log(num ** 2);
+    num = +prompt("Please enter a number");
 }
