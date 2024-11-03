b42c3fafb7dada69cc4b19292468ee9c701e830a
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 13:03:44 +0200

    initial

---
+++ 2024-11-03-javascript/04-conditions-if-else/assets/js/app.js
@@ -0,0 +1,10 @@
+const yourAge = prompt(`Please enter your age : `);
+// document.write(`Your age is : ${yourAge}`)
+
+if(yourAge >= 13){
+    alert("Welcome");
+    alert("Today we have sale on Artiks")
+} else {
+    alert("You don't have the requirement age.");
+    alert("and next time bring mama with you");
+}
---
+++ 2024-11-03-javascript/05-if-else-exercise/assets/js/app.js
@@ -0,0 +1,12 @@
+const num1 = +prompt(`Please enter a number : `);
+const num2 = +prompt(`Please another number : `);
+
+// parseInt() - Integer 
+// parseFloat() - Float
+// + - is a conversion operator if placed before a string operand
+
+if(num1 > num2){
+    console.log(num1);
+} else {
+    console.log(num2);
+}
