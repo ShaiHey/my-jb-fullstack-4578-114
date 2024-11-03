0e442b237cf2814c6e0922046e69cc15b87f9f0d
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 14:54:41 +0200

    initial

---
+++ 2024-11-03-javascript/11-power/assets/js/app.js
@@ -0,0 +1,14 @@
+let i = 1
+
+while (i !== 0) {
+    let num = +prompt("Please enter a number")
+    const result = num ** 2
+    if(num !== 0){
+        console.log(result)
+    }
+    i = num
+}
+
+
+// A variable without any assignment will be equal to a special JavaScript value, called undefined
+let num;
