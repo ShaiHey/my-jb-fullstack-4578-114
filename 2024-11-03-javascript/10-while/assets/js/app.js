2b259b9bec2d6b1bf2b54c390ac609eee0cf7423
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 14:39:54 +0200

    initial

---
+++ 2024-11-03-javascript/10-while/assets/js/app.js
@@ -0,0 +1,15 @@
+// Print all numbers 1-100
+let i = 1;
+
+while (i <= 100) {
+    document.write(`${i} `)
+    i++
+}
+
+// Print all odd numbers 2-100
+let j = 2;
+
+while (j <= 100) {
+    document.write(`${j} `)
+    j+=2
+}
