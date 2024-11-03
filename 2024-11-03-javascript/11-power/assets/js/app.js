49640c2b2cba2eae64cf33da745ab5bd4dcbced5
ShaiHey <chaiheyman1010@gmail.com>
2024-11-03 14:59:31 +0200

    initial

--- 2024-11-03-javascript/11-power/assets/js/app.js
+++ 2024-11-03-javascript/11-power/assets/js/app.js
@@ -3,9 +3,7 @@ let i = 1
 while (i !== 0) {
     let num = +prompt("Please enter a number")
     const result = num ** 2
-    if(num !== 0){
-        console.log(result)
-    }
+    console.log(result)
     i = num
 }
 
