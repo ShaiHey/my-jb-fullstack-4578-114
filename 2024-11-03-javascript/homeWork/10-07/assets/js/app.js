9ff5772decfdf09ddf3b8d56defd20d1f07bf07e
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 15:07:18 +0200

    homework10;07

---
+++ 2024-11-03-javascript/homeWork/10-07/assets/js/app.js
@@ -0,0 +1,6 @@
+const num1 = +prompt("Please enter a number:")
+const num2 = +prompt("Please enter another number:")
+const num3 = +prompt("Please enter another number:")
+const maxNum = Math.max(num1, num2, num3);
+
+document.write(`The max number is : ${maxNum}`)
