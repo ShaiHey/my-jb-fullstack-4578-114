7fd4ea31bc00e78b6cbbb081326563e9a1395d62
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 14:27:26 +0200

    homework10;01-02

---
+++ 2024-11-03-javascript/homeWork/10-01/assets/js/app.js
@@ -0,0 +1,6 @@
+const num1 = +prompt(`Please enter a number : `);
+const num2 = +prompt(`Please enter another number : `);
+
+const message = num1 > num2 ? num1 : num2;
+
+document.write(message);
---
+++ 2024-11-03-javascript/homeWork/10-02/assets/js/app.js
@@ -0,0 +1,5 @@
+const grade = +prompt(`Please enter a grade : `);
+
+const message = grade >= 60 ? "Pass" : "Fail";
+
+document.write(message);
