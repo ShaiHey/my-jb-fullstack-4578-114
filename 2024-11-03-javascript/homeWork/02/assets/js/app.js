b811688d44c3b65a38d43a4a79fc9fdbb3bc8c13
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 09:54:10 +0200

    homework02

---
+++ 2024-11-03-javascript/homeWork/02/assets/js/app.js
@@ -0,0 +1,5 @@
+let num = +prompt("Please enter a number");
+let num2 = +prompt("Please enter another number");
+let num3 = +prompt("Please enter another number");
+const result = num * num2 * num3
+document.write(`The product of ${num} * ${num2} * ${num3} is : ${result}`)
