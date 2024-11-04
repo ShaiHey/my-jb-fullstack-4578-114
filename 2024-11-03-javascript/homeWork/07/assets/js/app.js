eb15dd4ef7962014e9cb49efa1ce9a79bb76eb36
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 10:41:35 +0200

    homework07

---
+++ 2024-11-03-javascript/homeWork/07/assets/js/app.js
@@ -0,0 +1,5 @@
+const num = +prompt("Please enter a number:");
+const num2 = +prompt("Please enter another number:");
+const num3 = +prompt("Please enter another number:");
+const num4 = +prompt("Please enter another number:");
+document.write(`${num} + ${num2} + ${num3} + ${num4} = ${num+num2+num3+num4}`)
