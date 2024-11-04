21660226245138ad35410a9d00fd25c470ac16c9
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 10:50:19 +0200

    + added in prompt

--- 2024-11-03-javascript/homeWork/08/assets/js/app.js
+++ 2024-11-03-javascript/homeWork/08/assets/js/app.js
@@ -1,6 +1,6 @@
-const width = prompt("Please enter the width of the box:");
-const height = prompt("Please enter the height of the box:");
-const depth = prompt("Please enter the depth of the box:");
+const width = +prompt("Please enter the width of the box:");
+const height = +prompt("Please enter the height of the box:");
+const depth = +prompt("Please enter the depth of the box:");
 
 const volume = width * height * depth;
 document.write(`The volume of the box is: ${volume}`);
