010c6b7e985a40bf0da1f87ff1b7f282379d380c
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 10:00:23 +0200

    homework03

---
+++ 2024-11-03-javascript/homeWork/03/assets/js/app.js
@@ -0,0 +1,5 @@
+let num = +prompt("Please enter a number");
+let num2 = +prompt("Please enter another number");
+const result1 = num / num2
+const result2 = num2 / num
+document.write(`This is the two possible results:<br>${num} / ${num2} = ${result1}<br>${num2} / ${num} = ${result2}`)
