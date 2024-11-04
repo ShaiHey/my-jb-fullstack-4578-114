157c95bcd0094d010d628fa4bf8fbdf7bf8c9ac7
ShaiHey <chaiheyman1010@gmail.com>
2024-11-04 10:09:17 +0200

    homework05

---
+++ 2024-11-03-javascript/homeWork/05/assets/js/app.js
@@ -0,0 +1,6 @@
+const firstName = prompt("Please enter your first name:");
+const lastName = prompt("Please enter your last name:");
+const eMail = prompt("Please enter your email:");
+const numberPhone = prompt("Please enter your phone number:");
+alert(`Full Name : ${firstName} ${lastName}
+Email : ${eMail}, Phone : ${numberPhone}`)
