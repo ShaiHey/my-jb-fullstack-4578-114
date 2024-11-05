d89f01081bb6f014769a9c1f6467e68867931ee6
ShaiHey <chaiheyman1010@gmail.com>
2024-11-05 10:18:12 +0200

    power 3

--- 2024-11-05/03-do-while/assets/js/app.js
+++ 2024-11-05/03-do-while/assets/js/app.js
@@ -8,6 +8,6 @@
 let num;
 
 do {
-    num = +prompt();
+    num = +prompt("Please enter a number");
     alert(num ** 2);
 } while (num !== 0);
---
+++ 2024-11-05/04-power3/assets/js/app.js
@@ -0,0 +1,6 @@
+let num;
+
+do {
+    num = +prompt("Please enter a number");
+    alert(num ** 3);
+} while (num !== 0);
