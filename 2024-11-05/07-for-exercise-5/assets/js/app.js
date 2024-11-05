180dc0fb0cc2bec52016187cc725e899635ec38c
ShaiHey <chaiheyman1010@gmail.com>
2024-11-05 11:22:03 +0200

    for exercise 5

---
+++ 2024-11-05/07-for-exercise-5/assets/js/app.js
@@ -0,0 +1,13 @@
+// for(let i = 1; i <= 10; i++){
+//     for(let i = 1; i <= 10; i++){
+//         document.write("*")
+//     }
+//     document.write("<br>")
+// }
+
+for(let i = 1; i <= 10; i++){
+    for(let j = 1; j <= 10; j++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
