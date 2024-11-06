7621c4bcd94d2edf53f8b3aa8185ce053164466f
ShaiHey <chaiheyman1010@gmail.com>
2024-11-06 14:46:58 +0200

    homework11;10

---
+++ 2024-11-03-javascript/homeWork/11-10/assets/js/app.js
@@ -0,0 +1,16 @@
+const num1 = +prompt("Please enter a number :");
+const num2 = +prompt("Please enter another number :");
+let start;
+let end;
+
+if(num1 < num2){
+    start = num1;
+    end = num2;
+} else {
+    start = num2;
+    end = num1;
+}
+
+for(let i = start; i <= end; i++){
+    document.write(`${i}<br>`);
+}
