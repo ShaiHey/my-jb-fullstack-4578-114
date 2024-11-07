e364535c411093bef8db22b0616860c2e43d5cf0
ShaiHey <chaiheyman1010@gmail.com>
2024-11-07 20:01:03 +0200

    homework15;loops

---
+++ 2024-11-05-javascript-objects-and-arrays/homework/15-loops/assets/js/app.js
@@ -0,0 +1,8 @@
+let num = prompt("Please enter a number :");
+let i = parseInt(num);
+
+while (i > 0) {
+    console.log(num.length);
+    num = prompt("Please enter a number :");
+    i = parseInt(num);
+}
