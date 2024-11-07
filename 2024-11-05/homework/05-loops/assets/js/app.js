bf9f2ead8d19cf18160e53126035697a3b216b91
ShaiHey <chaiheyman1010@gmail.com>
2024-11-07 16:27:57 +0200

    homework05;loops

---
+++ 2024-11-05/homework/05-loops/assets/js/app.js
@@ -0,0 +1,8 @@
+let num = +prompt("Please enter a number :");
+
+for(let i = 1; i <= num; i++){
+    for(let j = 1; j <= num; j++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
