17afb47859eec70cbb6cde9b368644828db1fdf7
ShaiHey <chaiheyman1010@gmail.com>
2024-11-10 09:32:38 +0200

    use ternary

--- 2024-11-05-javascript-objects-and-arrays/homework/13-loops/assets/js/app.js
+++ 2024-11-05-javascript-objects-and-arrays/homework/13-loops/assets/js/app.js
@@ -1,17 +1,12 @@
-let width = +prompt("Please enter a number :"); // Number of *
-let height = +prompt("Please enter a number :"); // Number of line
+const width = +prompt("Please enter a number :"); // Number of *
+const height = +prompt("Please enter a number :"); // Number of line
 
 for(let i = 1; i <= height; i++){
     for(let j = 1; j <= width; j++){
-        if(i === 1 || i === height){
-            document.write("*")
-        } else {
-            if(j === 1 || j === width){
-                document.write("*");
-            } else {
-                document.write("&nbsp;&nbsp;");
-            }
-        }
+        const isFirstOrLastRow = i === 1 || i === height
+        const isFirstOrLastCol = j === 1 || j === width
+        const message = isFirstOrLastRow || isFirstOrLastCol ? "*" : "&nbsp;&nbsp;"
+        document.write(message)
     }
     document.write("<br>")
 }
