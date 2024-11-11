4383d92a4a8d70ecf2c2e6400812a4899f3a3c8a
ShaiHey <chaiheyman1010@gmail.com>
2024-11-11 10:57:37 +0200

    homework03;strings

---
+++ 2024-11-10/homework/03-strings/assets/js/app.js
@@ -0,0 +1,4 @@
+const phoneNumber = prompt("Please enter a phone number:");
+
+const message = phoneNumber.length === 10 && phoneNumber.startsWith("05") && !isNaN(phoneNumber) ? "Valid phone number" : "No valid";
+document.write(message);
