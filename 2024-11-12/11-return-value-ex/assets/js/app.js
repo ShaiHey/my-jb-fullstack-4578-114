323c87ad777adcfb2530860874fcddf1254e74b5
ShaiHey <chaiheyman1010@gmail.com>
2024-11-12 14:27:51 +0200

    return value ex

---
+++ 2024-11-12/11-return-value-ex/assets/js/app.js
@@ -0,0 +1,14 @@
+function getIsPositive(number) {
+    return number > 0;
+}
+
+const isPositive = getIsPositive(20);
+console.log(isPositive);
+
+// function getIsPositive(number) {
+//     if(num > 0) return true;
+//     return false;
+// }
+
+// const isPositive = getIsPositive(20);
+// console.log(isPositive);
