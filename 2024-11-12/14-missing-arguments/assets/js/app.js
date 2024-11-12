c11746d2be89d08e6452a5d29ccf73530c69b947
ShaiHey <chaiheyman1010@gmail.com>
2024-11-12 15:16:19 +0200

    missing arguments

--- 2024-11-12/14-missing-arguments/assets/js/app.js
+++ 2024-11-12/14-missing-arguments/assets/js/app.js
@@ -1,5 +1,10 @@
 function getAbs(num) {
+    // Protect the function from invalid arguments
+    if(typeof num !== "number") return;
     return num > 0 ? num : -num;
 }
 
-console.log(getAbs());
+console.log(getAbs); // Reference a function without (), is reference to function
+console.log(getAbs(-3)); // Proper invocation
+console.log(getAbs("Shai")); // Improper invocation - we can't expect logic results if we 
+console.log(getAbs(true)); // Proper invocation
