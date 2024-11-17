790533d75cee28f049afb585ff767cbd28ff5db6
ShaiHey <chaiheyman1010@gmail.com>
2024-11-17 13:50:28 +0200

    html events

---
+++ 2024-11-17/05-html-events/assets/js/app.js
@@ -0,0 +1,13 @@
+function sayGoodbye() {
+    alert("Goodbye :)");
+}
+
+function printRandomNumber() {
+    console.log(parseInt(Math.random() * 100));
+}
+
+function jibrish() {
+    console.log("emfleelfe");
+}
+
+console.log("js loaded....");
