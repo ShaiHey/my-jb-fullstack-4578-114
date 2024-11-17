7ace7e1bf89ec2bed8531341902b24de4858fdc9
ShaiHey <chaiheyman1010@gmail.com>
2024-11-17 15:56:13 +0200

    html validation

--- 2024-11-17/09-html-validation/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-function changeParagraphInnerText() {
-    document.getElementById("myP").innerText = document.getElementById("textInput").value;
-}
-
-function changeParagraphInnerHTML() {
-    document.getElementById("myP2").innerHTML = document.getElementById("textInput2").value;
-}
