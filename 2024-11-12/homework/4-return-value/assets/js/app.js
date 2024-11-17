b0cd46d16151c73d4c72792d547206f85bc230fe
ShaiHey <chaiheyman1010@gmail.com>
2024-11-17 09:20:58 +0200

    homework return 4

---
+++ 2024-11-12/homework/4-return-value/assets/js/app.js
@@ -0,0 +1,16 @@
+function getMaxLength(arr) {
+    let lastWord = "";
+    for(const item of arr){
+        lastWord = item.length > lastWord.length ? item : lastWord;
+    }
+
+    return lastWord;
+}
+
+const array1 = ["Test", "Shai", "Phone"];
+const array2 = ["Ran", "Cake", "Function"];
+const array3 = ["Pizza", "Matrix", "Water"];
+
+document.write(`Max length is : ${getMaxLength(array1)}<br>`);
+document.write(`Max length is : ${getMaxLength(array2)}<br>`);
+document.write(`Max length is : ${getMaxLength(array3)}<br>`);
