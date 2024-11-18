1cd23d3020c41e0da3e0f5d1a6063a6da6f79447
ShaiHey <chaiheyman1010@gmail.com>
2024-11-18 09:55:03 +0200

    homework dom 02

---
+++ 2024-11-17/homework/02-dom/assets/js/app.js
@@ -0,0 +1,9 @@
+function sum() {
+    const result = parseInt(document.getElementById('num').value) + parseInt(document.getElementById('num2').value) + parseInt(document.getElementById('num3').value);
+    alert(`Sum is : ${result}`);
+}
+
+function average() {
+    const result = parseInt(document.getElementById('num').value) + parseInt(document.getElementById('num2').value) + parseInt(document.getElementById('num3').value);
+    alert(`Average is : ${result/3}`);
+}
