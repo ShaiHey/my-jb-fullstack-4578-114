81ea9d583dfff96e164d35e4191d8f9bff76f2ad
ShaiHey <chaiheyman1010@gmail.com>
2024-11-19 15:52:46 +0200

    dhtml ex complete

--- 2024-11-19/02-DHTML-ex1/assets/js/app.js
+++ 2024-11-19/02-DHTML-ex1/assets/js/app.js
@@ -3,19 +3,55 @@ function addCarToTable(event) {
     event.preventDefault();
 
     const carsContainer = document.getElementById("carsContainer");
-    const carName = document.getElementById("carName").value;
-    const volumeMotor = document.getElementById("volumeMotor").value;
-    const color = document.getElementById("color").value;
-    const urlImage = document.getElementById("url").value;
-    const newLine = `<tr id="${carName}">
-                <th>${carName}</th>
-                <td>${volumeMotor}</td>
-                <td><img src="${urlImage}"></td>
-            </tr>`
+    const carName = document.getElementById("carName");
+    const volumeMotor = document.getElementById("volumeMotor");
+    const color = document.getElementById("color");
+    const urlImage = document.getElementById("url");
+    const newLine = `
+    <tr style="background-color: ${color.value};">
+        <th>${carName.value}</th>
+        <td>${volumeMotor.value}</td>
+        <td><img src="${urlImage.value}"></td>
+    </tr>`
+
+    const arr = {
+        carName: carName.value,
+        volumeMotor: volumeMotor.value,
+        color: color.value,
+        urlImage: urlImage.value
+    }
+
+    if(localStorage.getItem("cars2")){
+        const cars = JSON.parse(localStorage.getItem("cars2"));
+        cars.push(arr)
+        localStorage.setItem("cars2", JSON.stringify(cars));
+    } else {
+        const arr2 = [arr]
+        localStorage.setItem("cars2", JSON.stringify(arr2));
+    }
     
 
     carsContainer.innerHTML += newLine;
 
-    const lineTable = document.getElementById(carName);
-    lineTable.style.backgroundColor = color
+    const carForm = document.getElementById("carForm");
+    carForm.reset();
+    carName.focus();
 }
+
+function loadCarsToTable() {
+    if(localStorage.getItem("cars2")){
+        const cars = JSON.parse(localStorage.getItem("cars2"));
+        const carsContainer = document.getElementById("carsContainer");
+        for(const car of cars) {
+            const newLine = `
+                <tr style="background-color: ${car.color};">
+                    <th>${car.carName}</th>
+                    <td>${car.volumeMotor}</td>
+                    <td><img src="${car.urlImage}"></td>
+                </tr>`
+            carsContainer.innerHTML += newLine;
+        }
+    }
+}
+
+loadCarsToTable()
--- 2024-11-19/03-DHTML-ex1-2/assets/js/app.js
+++ 2024-11-19/03-DHTML-ex1-2/assets/js/app.js
@@ -1,21 +1,70 @@
-function addCarToTable(event) {
-
-    event.preventDefault();
-
-    const carsContainer = document.getElementById("carsContainer");
+function collectData() {
     const carName = document.getElementById("carName").value;
-    const volumeMotor = document.getElementById("volumeMotor").value;
+    const engineVolume = document.getElementById("volumeMotor").value;
     const color = document.getElementById("color").value;
-    const urlImage = document.getElementById("url").value;
-    const newLine = `<tr id="${carName}">
-                <th>${carName}</th>
-                <td>${volumeMotor}</td>
-                <td><img src="${urlImage}"></td>
-            </tr>`
-    
+    const image = document.getElementById("url").value;
 
-    carsContainer.innerHTML += newLine;
-
-    const lineTable = document.getElementById(carName);
-    lineTable.style.backgroundColor = color
+    return {
+        carName: carName,
+        engineVolume: engineVolume,
+        color: color,
+        image: image
+    }
 }
+
+function saveToStorage(data) {
+    if(localStorage.getItem("cars")) {
+        const arr = JSON.parse(localStorage.getItem("cars"));
+        arr.push(data);
+        localStorage.setItem("cars", JSON.stringify(arr));
+    } else {
+        const arr2 = [data];
+        localStorage.setItem("cars", JSON.stringify(arr2));
+    }
+}
+
+function generateHTML(data) {
+    const newHTML = `
+    <tr style="background-color: ${data.color};">
+        <td>${data.carName}</td>
+        <td>${data.engineVolume}</td>
+        <td><img src="${data.image}"></td>
+    </tr>`
+    
+    return newHTML
+}
+
+function renderHTML(newHTML) {
+    const carsContainer = document.getElementById("carsContainer");
+    carsContainer.innerHTML += newHTML;
+}
+
+function clearForm() {
+    const carForm = document.getElementById("carForm");
+    carForm.reset();
+
+    const carNameInput = document.getElementById("carName");
+    carNameInput.focus();
+}
+
+function addCarToTable(event) {
+    event.preventDefault();
+    const data = collectData();
+    const newHTML = generateHTML(data);
+    saveToStorage(data)
+    renderHTML(newHTML);
+    clearForm();
+}
+
+function addAllCars() {
+    const carsJSON = localStorage.getItem("cars");
+    if(carsJSON) {
+        const cars = JSON.parse(localStorage.getItem("cars"));
+        for(const car of cars){
+            const newHTML = generateHTML(car);
+            renderHTML(newHTML);
+        }
+    }
+}
+
+addAllCars();
