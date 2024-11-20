5555449a61808952f1da10446ee744785aa581d4
ShaiHey <chaiheyman1010@gmail.com>
2024-11-20 14:21:53 +0200

    homework dynamic html 04

---
+++ 2024-11-19/homework/04-dynamicHTML/assets/js/app.js
@@ -0,0 +1,48 @@
+let images = 0;
+
+function collectData() {
+    const description = document.getElementById("description").value;
+    const url = document.getElementById("url").value;
+
+    return {
+        description: description,
+        url: url
+    }
+}
+
+function generateNewHTML(data) {
+    const newHTMLine = `
+    <tr>
+        <th>${data.description}</th>
+        <th><img src="${data.url}"></th>
+    </tr>
+    `
+    return newHTMLine
+}
+
+function insertNewHTML(newHTML) {
+    const imagesContainer = document.getElementById("imagesContainer");
+    imagesContainer.innerHTML += newHTML;
+}
+
+function clearForm() {
+    const formImages = document.getElementById("formImages");
+    const description = document.getElementById("description");
+    formImages.reset();
+    description.focus();
+}
+
+function updateImage() {
+    images++;
+    const amountImage = document.getElementById("amountImage");
+    amountImage.innerHTML = `Amount images : ${images}`;
+}
+
+function addImage(event) {
+    event.preventDefault();
+    const data = collectData();
+    const newHTML = generateNewHTML(data);
+    insertNewHTML(newHTML);
+    updateImage();
+    clearForm();
+}
