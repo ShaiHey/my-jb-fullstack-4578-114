a519b063673ef45f4a699fca3eba358a024acc92
ShaiHey <chaiheyman1010@gmail.com>
2024-11-20 14:02:57 +0200

    homework dynamic html 03

---
+++ 2024-11-19/homework/03-dynamicHTML/assets/js/app.js
@@ -0,0 +1,44 @@
+const colors = [];
+
+function collectData() {
+    const color = document.getElementById("color").value;
+
+    return color;
+}
+
+function generateNewHTML(color) {
+    const newHTMLine = `
+    <tr>
+        <th>${color}</th>
+        <th class="colors" style="background-color: ${color};"></th>
+    </tr>
+    `
+    return newHTMLine
+}
+
+function insertNewHTML(newHTML) {
+    const colorsContainer = document.getElementById("colorsContainer");
+    colorsContainer.innerHTML += newHTML;
+}
+
+function clearForm() {
+    const formColors = document.getElementById("formColors");
+    const color = document.getElementById("color");
+    formColors.reset();
+    color.focus();
+}
+
+function updateColor(color) {
+    colors.push(parseInt(color));
+    const amountColor = document.getElementById("amountColor");
+    amountColor.innerHTML = `Amount color : ${colors.length}`;
+}
+
+function addColor(event) {
+    event.preventDefault();
+    const data = collectData();
+    const newHTML = generateNewHTML(data);
+    insertNewHTML(newHTML);
+    updateColor(data);
+    clearForm();
+}
