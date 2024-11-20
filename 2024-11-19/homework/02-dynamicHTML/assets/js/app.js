bb325b34263a90e7ebe5e2120ea40ed9a0d82db9
ShaiHey <chaiheyman1010@gmail.com>
2024-11-20 13:43:10 +0200

    homework dynamic html 02

---
+++ 2024-11-19/homework/02-dynamicHTML/assets/js/app.js
@@ -0,0 +1,58 @@
+const grades = [];
+
+function collectData() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const grade = document.getElementById("grade").value;
+
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        grade: grade
+    }
+}
+
+function generateNewHTML(data) {
+    const newHTMLine = `
+    <tr>
+        <th>${data.firstName}</th>
+        <th>${data.lastName}</th>
+        <th>${data.grade}</th>
+    </tr>
+    `
+    return newHTMLine
+}
+
+function insertNewHTML(newHTML) {
+    const gradesContainer = document.getElementById("gradesContainer");
+    gradesContainer.innerHTML += newHTML;
+}
+
+function clearForm() {
+    const formGrades = document.getElementById("formGrades");
+    const firstName = document.getElementById("firstName");
+    formGrades.reset();
+    firstName.focus();
+}
+
+function updateAverage(data) {
+    grades.push(parseInt(data.grade));
+
+    let sum = 0;
+    for(const grade of grades) {
+        sum += grade
+    }
+
+    const gradeAverage = document.getElementById("gradeAverage");
+
+    gradeAverage.innerHTML = `Average is : ${sum/grades.length}`;
+}
+
+function addIngredient(event) {
+    event.preventDefault();
+    const data = collectData();
+    const newHTML = generateNewHTML(data);
+    insertNewHTML(newHTML);
+    updateAverage(data);
+    clearForm();
+}
