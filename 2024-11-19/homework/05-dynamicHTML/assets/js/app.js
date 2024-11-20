2f10f5dd352cda2cf66046644a3b7555048b4f7c
ShaiHey <chaiheyman1010@gmail.com>
2024-11-20 14:41:09 +0200

    homework dynamic html 05

---
+++ 2024-11-19/homework/05-dynamicHTML/assets/js/app.js
@@ -0,0 +1,68 @@
+function collectData() {
+    const animalType = document.getElementById("animalType").value;
+    const animalName = document.getElementById("animalName").value;
+    const animalAge = document.getElementById("animalAge").value;
+    const animalImage = document.getElementById("animalImage").value;
+
+    return {
+        animalType: animalType,
+        animalName: animalName,
+        animalAge: animalAge,
+        animalImage: animalImage
+    }
+}
+
+function generateNewHTML(data) {
+    const newHTMLine = `
+    <tr>
+        <th>${data.animalType}</th>
+        <th>${data.animalName}</th>
+        <th>${data.animalAge}</th>
+        <th><img src="${data.animalImage}"></th>
+    </tr>
+    `
+    return newHTMLine
+}
+
+function insertNewHTML(newHTML) {
+    const animalsContainer = document.getElementById("animalsContainer");
+    animalsContainer.innerHTML += newHTML;
+}
+
+function clearForm() {
+    const formAnimals = document.getElementById("formAnimals");
+    const animalType = document.getElementById("animalType");
+    formAnimals.reset();
+    animalType.focus();
+}
+
+function addToStorage(data) {
+    if(localStorage.getItem("animals")){
+        const currentAnimals = JSON.parse(localStorage.getItem("animals"));
+        currentAnimals.push(data);
+        localStorage.setItem("animals", JSON.stringify(currentAnimals))
+    } else {
+        localStorage.setItem("animals", JSON.stringify([data]))
+    }
+}
+
+function addAnimal(event) {
+    event.preventDefault();
+    const data = collectData();
+    const newHTML = generateNewHTML(data);
+    insertNewHTML(newHTML);
+    addToStorage(data);
+    clearForm();
+}
+
+function loadAnimals() {
+    if(localStorage.getItem("animals")){
+        const animals = JSON.parse(localStorage.getItem("animals"));
+        for(const animal of animals) {
+            const newHTML = generateNewHTML(animal);
+            insertNewHTML(newHTML);
+        }
+    }
+}
+
+loadAnimals();
