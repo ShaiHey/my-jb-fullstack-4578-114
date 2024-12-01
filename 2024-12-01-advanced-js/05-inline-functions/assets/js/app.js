889fad1e7cc3879a792a94407cfebc8db23a96ea
ShaiHey <chaiheyman1010@gmail.com>
2024-12-01 10:20:11 +0200

    inline function

---
+++ 2024-12-01-advanced-js/05-inline-functions/assets/js/app.js
@@ -0,0 +1,9 @@
+function printPower2(num, powerFunc) {
+    if(typeof powerFunc !== "function") return;
+    console.log(powerFunc(num));
+}
+
+// if i want to use a callback that unique, which i don't need
+// anywhere else in the code
+// i can declare it inside the function call, and this is called inline function
+printPower2(3, function power2(num) {return num ** 2;}); // inline function
