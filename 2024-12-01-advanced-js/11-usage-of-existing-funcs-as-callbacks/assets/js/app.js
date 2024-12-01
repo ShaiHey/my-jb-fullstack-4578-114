3b6e8987a55fd5d131ea9ae0215cc97e965f06c1
ShaiHey <chaiheyman1010@gmail.com>
2024-12-01 13:53:58 +0200

    existing functions callbacks

---
+++ 2024-12-01-advanced-js/11-usage-of-existing-funcs-as-callbacks/assets/js/app.js
@@ -0,0 +1,9 @@
+// const showUserLocation = () => {
+//     navigator.geolocation.getCurrentPosition(myLocation => console.log(myLocation));
+// }
+
+// showUserLocation();
+
+// navigator.geolocation.getCurrentPosition(myLocation => console.log(myLocation));
+
+navigator.geolocation.getCurrentPosition(console.log);
