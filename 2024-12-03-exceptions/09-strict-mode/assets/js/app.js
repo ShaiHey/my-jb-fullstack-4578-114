4694667366090632707971ec1991d253bdb3ce5f
ShaiHey <chaiheyman1010@gmail.com>
2024-12-03 14:26:55 +0200

    strict mode

---
+++ 2024-12-03-exceptions/09-strict-mode/assets/js/app.js
@@ -0,0 +1,37 @@
+// const message = "Hello World !";
+
+// console.log(message);
+// console.log(message[1]);
+// message[1] = "o"
+// console.log(message);
+
+
+// let counter = 3;
+// console.log(counter);
+// count = 4;
+// alert = message => console.log(message);
+// console.log(counter);
+
+
+// alert(window.count);
+// alert("Should be alerted but we see it in console instead");
+
+"use strict";
+
+const message = "Hello World !";
+
+console.log(message);
+console.log(message[1]);
+// message[1] = "o"
+console.log(message);
+
+
+let counter = 3;
+console.log(counter);
+// count = 4;
+alert = message => console.log(message);
+console.log(counter);
+
+
+alert(window.count);
+alert("Should be alerted but we see it in console instead");
