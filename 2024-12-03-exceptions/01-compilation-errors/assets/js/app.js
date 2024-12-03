58359faa3fba2f64f4ca821f5c8bd012a1a33d2b
ShaiHey <chaiheyman1010@gmail.com>
2024-12-03 11:04:05 +0200

    compilation, runtime, logical errors

---
+++ 2024-12-03-exceptions/01-compilation-errors/assets/js/app.js
@@ -0,0 +1,7 @@
+// This is a ""Compilation error""
+console.log("Hello");
+// const firstName = "Shai;
+console.log("Hello again");
+console.log(firstName);
+
+// ""Compilation errors"" will result in a completely not running code
---
+++ 2024-12-03-exceptions/02-runtime-errors/assets/js/app.js
@@ -0,0 +1,9 @@
+const numberOfDigits = +prompt("Please enter number of digits");
+
+console.log("Hello 1");
+
+const pi = Math.PI.toFixed(numberOfDigits);
+
+console.log("Hello 2");
+
+console.log(pi);
