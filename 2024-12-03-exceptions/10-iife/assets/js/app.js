02dfbc4f8a0c11585f862217ac2507a3baa411df
ShaiHey <chaiheyman1010@gmail.com>
2024-12-03 15:12:44 +0200

    IIFE

---
+++ 2024-12-03-exceptions/10-iife/assets/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+(() => {
+  function print() {
+    console.log("Printing....")
+  }
+  print();
+
+  function alert(msg) {
+    console.log(msg);
+  }
+  alert("This is a message");
+
+  function doPrint() {
+    console.log("sfgsdghfggh");
+  }
+
+  doPrint();
+  
+})()
---
+++ 2024-12-03-exceptions/10-iife/assets/js/printing.js
@@ -0,0 +1,9 @@
+// IIFE = Immediately Invoked Function Expression
+"use strict";
+
+(() => {
+    function doPrint() {
+        console.log("Do printing....");
+    }
+    doPrint();
+})()
