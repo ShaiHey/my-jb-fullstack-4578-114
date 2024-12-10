09f2a092a3adc5043a481d1c86c8811ba65d5bfe
ShaiHey <chaiheyman1010@gmail.com>
2024-12-10 15:38:08 +0200

    todos

---
+++ 2024-12-10-ajax/08-todos-exercise3/assets/js/app.js
@@ -0,0 +1,112 @@
+"use strict";
+
+(() => {
+
+  const getData = async url => {
+    return fetch(url).then(response => response.json());
+  }
+
+  const generateUsersTable = todos => {
+    const result = todos.reduce((acc, {userId, completed}) => {
+
+      let existingObjectIndex = acc.findIndex(user => user.userId === userId);
+      if(existingObjectIndex === -1) {
+        acc.push({
+          userId,
+          completed: 0,
+          incompleted: 0
+        })
+        existingObjectIndex = acc.findIndex(user => user.userId === userId);
+      }
+      acc[existingObjectIndex].completed += completed ? 1 : 0;
+      acc[existingObjectIndex].incompleted += completed ? 0 : 1
+      return acc;
+    }, []).map(({userId, completed, incompleted}) => `
+          <tr>
+            <td>${userId}</td>
+            <td>${completed}</td>
+            <td>${incompleted}</td>
+          </tr>
+      `)
+      .reduce((acc, current) => `${acc}${current}`, '')
+
+    console.log(result);
+
+    return result;
+  }
+
+  const generateStatsTable = todos => {
+    const totalTodos = todos.length;
+    const totalCompletedTodos = todos.filter(todo => todo.completed).length;
+    const completeRatio = totalCompletedTodos/totalTodos * 100;
+    return `
+        <tr>
+          <td>Total todos :</td>
+          <td>${totalTodos}</td>
+        </tr>
+
+        <tr>
+          <td>Total Completed todos :</td>
+          <td>${totalCompletedTodos}</td>
+        </tr>
+
+        <tr>
+          <td>Total Incomplete todos :</td>
+          <td>${totalTodos - totalCompletedTodos}</td>
+        </tr>
+
+        <tr>
+          <td>Complete Ratio :</td>
+          <td>${completeRatio}%</td>
+        </tr>
+    `
+  }
+
+  const generateTodosTable = todos => {
+    const newHTML = todos // ${completed ? "assets/images/check.png" : "assets/images/x.png"}
+      .map(({userId, title, completed}) => `
+          <tr>
+            <td>${userId}</td>
+            <td>${title}</td>
+            <td>${completed ? "Yes" : "No"}</td>
+          </tr>
+      `)
+      .reduce((acc, current) => `${acc}${current}`, '')
+    return newHTML;
+  }
+
+  const renderTodosTable = newHTML => document.getElementById("todosContainer").innerHTML = newHTML;
+  const renderStatsTable = newHTML => document.getElementById("todosStats").innerHTML = newHTML;
+  const renderStatsTablePerUser = newHTML => document.getElementById("todosStatsPerUser").innerHTML = newHTML;
+
+  document.getElementById("displayTodos").addEventListener("click", async () => {
+    try {
+      // Get data
+      const todos = await getData("https://jsonplaceholder.typicode.com/todos");
+
+      // Generate HTML
+      const todosHTML = generateTodosTable(todos);
+      const statsHTML = generateStatsTable(todos);
+      const statsUserHTML = generateUsersTable(todos);
+
+      // Render HTML
+      renderTodosTable(todosHTML);
+      renderStatsTable(statsHTML);
+      renderStatsTablePerUser(statsUserHTML);
+    } catch (error) {
+      console.warn(error);
+    }
+  })
+
+  // document.getElementById("displayTodosUser").addEventListener("click", async () => {
+  //   try {
+  //     const userId = +document.getElementById("userId").value;
+      
+  //     const todoPerUser = await getData(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`);
+  //     const statsPerUserHTML = generateStatsTable(todoPerUser);
+  //     renderStatsTablePerUser(statsPerUserHTML);
+  //   } catch (error) {
+  //     console.warn(error);
+  //   }
+  // });
+})();
