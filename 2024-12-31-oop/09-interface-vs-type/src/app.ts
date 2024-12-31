c8965d0a34c8775deceb72d8f22cf1a7474a8d11
ShaiHey <chaiheyman1010@gmail.com>
2024-12-31 14:11:21 +0200

    interface vs type

---
+++ 2024-12-31-oop/09-interface-vs-type/src/app.ts
@@ -0,0 +1,16 @@
+import CoffeeMakerInterface from "./coffee-maker-interface.js";
+import CoffeeMakerType from "./coffee-maker-type.js";
+
+const coffeeMaker: CoffeeMakerType = {
+    numberOfMilkHotters: 2,
+    numberOfPins: 4,
+    make: "Lavaza",
+    voltage: 110
+}
+
+const anotherCoffeeMaker: CoffeeMakerInterface = {
+    numberOfMilkHotters: 2,
+    numberOfPins: 4,
+    make: "Lavaza",
+    voltage: 110
+}
