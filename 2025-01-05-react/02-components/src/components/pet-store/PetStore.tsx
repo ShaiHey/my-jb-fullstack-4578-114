3c9532e7371fa9aaf6fa8364d8ec73f4d589c0f2
ShaiHey <chaiheyman1010@gmail.com>
2025-01-05 12:05:51 +0200

    components

---
+++ 2025-01-05-react/02-components/src/components/pet-store/PetStore.tsx
@@ -0,0 +1,12 @@
+import './PetStore.css'
+
+function PetStore() {
+    return (
+        <div className='PetStore'>
+            <p className='PetStore'>Hello Pet Store</p>
+        </div>
+    )
+}
+
+
+export default PetStore;
