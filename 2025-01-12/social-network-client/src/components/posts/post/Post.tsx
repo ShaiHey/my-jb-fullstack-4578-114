6636d4f7d5bfe3365b6857c49bc8aafaac93a75a
ShaiHey <chaiheyman1010@gmail.com>
2025-01-12 12:10:38 +0200

    post delete action

--- 2025-01-12/social-network-client/src/components/posts/post/Post.tsx
+++ 2025-01-12/social-network-client/src/components/posts/post/Post.tsx
@@ -1,13 +1,30 @@
 import PostModel from '../../../models/post/Post';
+import profile from '../../../services/profile';
 import './Post.css'
 
 interface PostProps {
-    post: PostModel
+    post: PostModel;
+    isAllowActions?: boolean;
+    remove?(id: string): void;
 }
 
-function Post({ post }: PostProps): JSX.Element {
-    const { title, body, createdAt} = post
+function Post({ post, isAllowActions, remove }: PostProps): JSX.Element {
+    const { id, title, body, createdAt} = post
     const { name } = post.user
+
+    async function deleteMe() {
+        if(remove && confirm(`Are you sure you want to delete "${title}"`)) {
+            try {
+                await profile.remove(id)
+                remove(id);
+                // I was able to delete from the server,
+                // this is the point to affect the UI
+                // In other words, we need to change the state
+            } catch (error) {
+                alert(error)
+            }
+        }
+    }
     
     return (
         <div className='Post'>
@@ -16,6 +33,12 @@ function Post({ post }: PostProps): JSX.Element {
                 by {name} at {createdAt}
             </div>
             <div>{body}</div>
+            {isAllowActions && 
+                <div>
+                    <button>Edit</button>
+                    <button onClick={deleteMe}>Delete</button>
+                </div>
+            }
         </div>
     )
 }
--- 2025-01-12/social-network-client/src/components/posts/profile/Profile.tsx
+++ 2025-01-12/social-network-client/src/components/posts/profile/Profile.tsx
@@ -25,12 +25,23 @@ function Profile(): JSX.Element {
         // })()
     }, [])
 
+    function remove(id: string): void {
+        const index = posts.findIndex(post => post.id === id);
+        if(index > -1) {
+            const postsAfterRemoval = [...posts]
+            postsAfterRemoval.splice(index, 1)
+            setPosts(postsAfterRemoval)
+        }
+    }
+
     return (
         <div className='Profile'>
             {posts.map(post =>
                 <Post
                     key={post.id}
                     post={post}
+                    remove={remove}
+                    isAllowActions={true}
                 >
                 </Post>)}
         </div>
