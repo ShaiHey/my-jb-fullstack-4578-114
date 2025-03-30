203bfbdb77b22412bef9ccc116db5a07e2aa61f2
ShaiHey <chaiheyman1010@gmail.com>
2025-03-30 14:58:20 +0300

    for empty

--- 2025-03-30-angular/02-social-network-client/src/app/components/posts/profile/profile.component.ts
+++ 2025-03-30-angular/02-social-network-client/src/app/components/posts/profile/profile.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { ProfileService } from '../../../services/profile.service';
+import { Post } from '../../../models/post/post.model';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ PostComponent ],
   // providers: [ ProfileService ], // if the components needs its own ProfileService, it states here like so...
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+
+  profile = signal<Post[]>([])
 
   constructor(
     public profileService: ProfileService
   ) {}
 
+  ngOnDestroy(): void {
+  }
+
   async ngOnInit(): Promise<void> {
-    const profile = await this.profileService.getProfile()
-    console.log(profile)
+    const profilePosts = await this.profileService.getProfile()
+    this.profile.set(profilePosts)
   }
 }
